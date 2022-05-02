c35e62b7cb24e6c21947a24624b623dcb42fc26c
Kovalev Danil <opana.loog@gmail.com>
2022-05-03 01:19:46 +0300

    udpate event type for audio

--- audio-model/src/main/java/ua/tunepoint/audio/model/event/AudioEventType.java
+++ audio-model/src/main/java/ua/tunepoint/audio/model/event/AudioEventType.java
@@ -2,18 +2,22 @@ package ua.tunepoint.audio.model.event;
 
 import lombok.AllArgsConstructor;
 import ua.tunepoint.audio.model.event.audio.AudioCreatedEvent;
+import ua.tunepoint.audio.model.event.audio.AudioDeletedEvent;
 import ua.tunepoint.audio.model.event.audio.AudioLikeEvent;
 import ua.tunepoint.audio.model.event.audio.AudioListenEvent;
 import ua.tunepoint.audio.model.event.audio.AudioUnlikeEvent;
+import ua.tunepoint.audio.model.event.audio.AudioUpdatedEvent;
 import ua.tunepoint.event.model.DomainEventType;
 
 @AllArgsConstructor
 public enum AudioEventType implements DomainEventType {
 
-    AUDIO_CREATE("create", AudioCreatedEvent.class),
-    AUDIO_LIKE("like", AudioLikeEvent.class),
-    AUDIO_UNLIKE("unlike", AudioUnlikeEvent.class),
-    AUDIO_LISTENING("listening", AudioListenEvent.class);
+    AUDIO_CREATE("created", AudioCreatedEvent.class),
+    AUDIO_UPDATED("updated", AudioUpdatedEvent.class),
+    AUDIO_DELETED("deleted", AudioDeletedEvent.class),
+    AUDIO_LIKE("liked", AudioLikeEvent.class),
+    AUDIO_UNLIKE("unliked", AudioUnlikeEvent.class),
+    AUDIO_LISTENING("listened", AudioListenEvent.class);
 
     private final String name;
     private final Class<?> type;
